dd09fa2f1a93ffc795b621fd9a9ebe469e3af450
gaoxueyao <gaoxy87@163.com>
2020-07-16 19:08:24 +0800

    add checkpoint dir config

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import configparser
 cfg_parser = configparser.ConfigParser()
-cfg_parser.read('hyper_param.ini')
+cfg_parser.read('config.ini')
 
-dropout_rate = cfg_parser.getfloat('default', 'dropout_rate', fallback=0.25)
-learning_rate = cfg_parser.getfloat('default', 'learning_rate', fallback=0.0001)
-batch_size = cfg_parser.getint('default', 'batch_size', fallback=32)
-checkpoint_dir = cfg_parser.get('default', 'checkpoint_dir', fallback='checkpoint')
 
-def print_hyper_params():
-    print(f'Hyper-params: dropout_rate={dropout_rate}, learning_rate={learning_rate}, batch_size={batch_size}')
+# Hyper-params:
+dropout_rate = cfg_parser.getfloat('hyper-param', 'dropout_rate', fallback=0.25)
+learning_rate = cfg_parser.getfloat('hyper-param', 'learning_rate', fallback=0.0001)
+batch_size = cfg_parser.getint('hyper-param', 'batch_size', fallback=32)
+
+# Environment
+checkpoint_dir = cfg_parser.get('env', 'checkpoint_dir', fallback='checkpoint')
 
 
 # char_set: remove "Il10oO", C->c, K->k, P->p, S->s, W->w, V->v, U->u, X->x, Y->y, Z->z
@@ -17,3 +18,12 @@ char_count = 4
 
 image_height = 48
 image_width = 128
+
+
+def print_hyper_params():
+    print(f'Hyper-params: dropout_rate={dropout_rate}, learning_rate={learning_rate}, batch_size={batch_size}')
+
+
+if __name__ == '__main__':
+    print_hyper_params()
+    print(f'checkpoint_dir={checkpoint_dir}')
